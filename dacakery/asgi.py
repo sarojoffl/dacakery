ae2e0bf692fad2a4c0bc56b9e83ba1c83f72734e
sarojoffl <sarojoffl@gmail.com>
2025-05-22 12:17:20 +0545

    Initial commit - setup Da Cakery Django project

---
+++ dacakery/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for dacakery project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dacakery.settings')
+
+application = get_asgi_application()
